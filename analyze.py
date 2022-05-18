1356226d5702b17b6938efe1b2f9f4b10bec1606
Maks Gontarek <m.gontarek@wrseti.pl>
2022-05-18 14:14:48 +0200

    Add questions pivot and concate

--- analyze.py
+++ analyze.py
@@ -13,9 +13,10 @@ pytania13_15 = pd.read_sql("select*from pytania13_15", dbConn)'''
 
 #Read files for testing purposes 
 ankietowany = pd.read_csv(r'static/ankietowany.csv', encoding='cp1252', sep=";")
-pytania1_4 = pd.read_csv(r'static/pytania1_4.csv', encoding='cp1252', sep=";")
-pytania5_12 = pd.read_csv(r'static/pytania5_12.csv', encoding='cp1252', sep=";")
-pytania13_15 = pd.read_csv(r'static/pytania13_15.csv', encoding='cp1252', sep=";")
+ankietowanyID = pd.read_csv(r'static/ankietowany.csv', encoding='cp1252', sep=";", usecols = ['ID'])
+pytania1_4 = pd.read_csv(r'static/pytania1_4.csv', encoding='cp1252', sep=";", usecols = ['Nr_Pytania', 'Odpowiedz', 'Person_ID'])
+pytania5_12 = pd.read_csv(r'static/pytania5_12.csv', encoding='cp1252', sep=";", usecols = ['Nr_Pytania', 'Odpowiedz', 'Person_ID'])
+pytania13_15 = pd.read_csv(r'static/pytania13_15.csv', encoding='cp1252', sep=";", usecols = ['Nr_Pytania', 'Odpowiedz', 'Person_ID'])
 
 #Prints how many rows have null values
 #print(len(ankietowany[pd.isnull(ankietowany.Zawod)]))
@@ -32,12 +33,19 @@ ankietowany['Zawod'] = ankietowany['Zawod'].str.strip()
 #Counting people without job
 print("Liczba osób bez zawodu: " + str(ankietowany.Zawod.str.count("Brak").sum()))
 
-print(ankietowany)
+#Pivot questions
+pytania1_4 = pytania1_4.pivot(index="Nr_Pytania", columns=["Person_ID"], values="Odpowiedz")
+pytania5_12 = pytania5_12.pivot(index="Nr_Pytania", columns=["Person_ID"], values="Odpowiedz")
+pytania13_15 = pytania13_15.pivot(index="Nr_Pytania", columns=["Person_ID"], values="Odpowiedz")
+
+#concate questions
+pytania = [pytania1_4, pytania5_12, pytania13_15]
+result = pd.concat(pytania)
 
 #Check if there's empty row and fill it
 if ankietowany['Zawod'].isnull:
     ankietowany['Zawod'] = ankietowany['Zawod'].fillna("Brak")
 
-'''
-#Write corrected version to csv
-ankietowany.to_csv(r'static/ankietowany_out.csv', encoding='cp1252', sep=";", index=False)'''
+#Write corrected and concated version to csv
+result.to_csv(r'static/result.csv', encoding='cp1252', sep=";")
+ankietowany.to_csv(r'static/ankietowany_out.csv', encoding='cp1252', sep=";", index=False)
