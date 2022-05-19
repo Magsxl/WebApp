5da99a9484b7b67eb42c53bd608fe296d282088f
Maks Gontarek <m.gontarek@wrseti.pl>
2022-05-19 23:09:48 +0200

    Add answers distribution

--- analyze.py
+++ analyze.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
-from sqlalchemy import create_engine
+from sqlalchemy import Column, create_engine
 '''
 sqlEngine = create_engine('mysql+pymysql://mgasxl:Najlepszy@magsxl.mysql.pythonanywhere-services.com/magsxl$ankieta', pool_recycle=3600)
 
@@ -18,9 +18,6 @@ ankietowany = pd.read_csv(r'static/ankietowany.csv', encoding='cp1252', sep=";")
 pytania1_4 = pd.read_csv(r'static/pytania1_4.csv', encoding='cp1252', sep=";", usecols = ['Nr_Pytania', 'Odpowiedz', 'Person_ID'])
 pytania5_12 = pd.read_csv(r'static/pytania5_12.csv', encoding='cp1252', sep=";", usecols = ['Nr_Pytania', 'Odpowiedz', 'Person_ID'])
 pytania13_15 = pd.read_csv(r'static/pytania13_15.csv', encoding='cp1252', sep=";", usecols = ['Nr_Pytania', 'Odpowiedz', 'Person_ID'])
-
-#Prints how many rows have null values
-#print(len(ankietowany[pd.isnull(ankietowany.Zawod)]))
 
 #Translate string values to int
 ankietowany.loc[ankietowany['Pochodzenie'].str.contains('250'), 'Pochodzenie'] = '250'
@@ -57,9 +54,7 @@ if ankietowany['Zawod'].isnull:
 result.to_csv(r'static/result.csv', encoding='cp1252', sep=";")
 ankietowany.to_csv(r'static/ankietowany_out.csv', encoding='cp1252', sep=";", index=False)
 
-#Data exploration
-#ankietowany.info()
-#print(ankietowany.describe())
+#DATA EXPLORATION
 '''
 for x in range(1,countCol):
     colName = result.columns[x]
@@ -75,3 +70,26 @@ f, ax = plt.subplots(figsize=(10,7))
 resultValPlot = sns.barplot(x='Odpowiedzi', y='Ilosc', palette="Blues_d", data=resultVal)
 resultValPlot.bar_label(resultValPlot.containers[0])
 plt.show()
+
+#Answers distribution
+ans1 = result.eq(1).sum(axis=1).to_frame().T
+ans2 = result.eq(2).sum(axis=1).to_frame().T
+ans3 = result.eq(3).sum(axis=1).to_frame().T
+ans4 = result.eq(4).sum(axis=1).to_frame().T
+ans5 = result.eq(5).sum(axis=1).to_frame().T
+
+#Plot answers distribution
+resPartPlot = sns.barplot(data=ans1).set(title='Zdecydowanie tak')
+plt.figure()
+resPartPlot2 = sns.barplot(data=ans2).set(title='Raczej tak')
+plt.figure()
+resPartPlot3 = sns.barplot(data=ans3).set(title='Nie wiem')
+plt.figure()
+resPartPlot4 = sns.barplot(data=ans4).set(title='Raczej nie')
+plt.figure()
+resPartPlot5 = sns.barplot(data=ans5).set(title='Zdecydowanie nie')
+plt.show()
+
+#Answers for first question
+#ques1 = result.eq(1).sum(axis=1).to_frame().T
+#print(result.loc[result[1]==1])
