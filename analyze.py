5435822ffe97e4633146162b730ffb1bed8c72b5
Maks Gontarek <m.gontarek@wrseti.pl>
2022-05-21 20:24:25 +0200

    Add more translation from string to int. Add getting all answers and join it with ankietowany. Add correlation

--- analyze.py
+++ analyze.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from sqlalchemy import create_engine
+from sklearn import preprocessing
 '''
 sqlEngine = create_engine('mysql+pymysql://mgasxl:Najlepszy@magsxl.mysql.pythonanywhere-services.com/magsxl$ankieta', pool_recycle=3600)
 
@@ -19,12 +20,6 @@ ankietowany = pd.read_csv(r'static/ankietowany.csv', encoding='cp1252', sep=";")
 pytania1_4 = pd.read_csv(r'static/pytania1_4.csv', encoding='cp1252', sep=";", usecols = ['Nr_Pytania', 'Odpowiedz', 'Person_ID'])
 pytania5_12 = pd.read_csv(r'static/pytania5_12.csv', encoding='cp1252', sep=";", usecols = ['Nr_Pytania', 'Odpowiedz', 'Person_ID'])
 pytania13_15 = pd.read_csv(r'static/pytania13_15.csv', encoding='cp1252', sep=";", usecols = ['Nr_Pytania', 'Odpowiedz', 'Person_ID'])
-
-#Translate string values to int
-ankietowany.loc[ankietowany['Pochodzenie'].str.contains('250'), 'Pochodzenie'] = '250'
-ankietowany.loc[ankietowany['Pochodzenie'].str.contains('100'), 'Pochodzenie'] = '100'
-ankietowany.loc[ankietowany['Pochodzenie'].str.contains('Miasteczko'), 'Pochodzenie'] = '30'
-ankietowany.loc[ankietowany['Pochodzenie'].str.contains('Wie'), 'Pochodzenie'] = '0'
 
 #Cleaning useless white spaces
 ankietowany['Zawod'] = ankietowany['Zawod'].str.strip()
@@ -47,6 +42,17 @@ for x in range(1,countCol):
     if not x in result.iloc[0]:
         ankietowany = ankietowany[ankietowany.ID != x]
 
+#Translate string values to int
+ankietowany.loc[ankietowany['Pochodzenie'].str.contains('250', na=False), 'Pochodzenie'] = 250
+ankietowany.loc[ankietowany['Pochodzenie'].str.contains('100', na=False), 'Pochodzenie'] = 100
+ankietowany.loc[ankietowany['Pochodzenie'].str.contains('Miasteczko', na=False), 'Pochodzenie'] = 30
+ankietowany.loc[ankietowany['Pochodzenie'].str.contains('Wie', na=False), 'Pochodzenie'] = 0
+ankietowany.loc[ankietowany['Plec'].str.contains('M', na=False), 'Plec'] = 0
+ankietowany.loc[ankietowany['Plec'].str.contains('F', na=False), 'Plec'] = 1
+ankietowany.loc[ankietowany['Status'].str.contains('Student', na=False), 'Status'] = 0
+ankietowany.loc[ankietowany['Status'].str.contains('Pracownik', na=False), 'Status'] = 1
+ankietowany.loc[ankietowany['Status'].str.contains('Ucz', na=False), 'Status'] = 2
+
 #Check if there's empty row and fill it
 if ankietowany['Zawod'].isnull:
     ankietowany['Zawod'] = ankietowany['Zawod'].fillna("Brak")
@@ -67,20 +73,19 @@ result.to_csv(r'static/result.csv', encoding='cp1252', sep=";")
 ankietowany.to_csv(r'static/ankietowany_out.csv', encoding='cp1252', sep=";", index=False)
 
 #Get only students from ankietowany
-ankStudent = ankietowany[ankietowany['Status']=='Student']
+ankStudent = ankietowany[ankietowany['Status']==0]
 ankStudent = ankStudent[['ID', 'Status']]
 
 #Get only workers from ankietowany
-ankPE = ankietowany[ankietowany['Status']=='Pracownik etatowy']
+ankPE = ankietowany[ankietowany['Status']==1]
 ankPE = ankPE[['ID', 'Status']]
-print(ankPE)
 
 #DATA EXPLORATION
 '''
 for x in range(1,countCol):
     colName = result.columns[x]
     if x in result.iloc[0]:
-        print("Answers for: " + str(colName) + "\n" + str(result[x].values))
+        print("Answers for: " + str(colName-1) + "\n" + str(result[x].values))
     else:
         print("Value: " + str(x) + " doesn't exist")
 '''
@@ -111,44 +116,43 @@ plt.figure()
 resPartPlot5 = sns.barplot(data=ans5).set(title='Zdecydowanie nie')
 plt.show()
 
-
 #Answers for every question
 quesDict = {
-    1: "Czy jest dla Ciebie ważna ochrona środowiska?",
-    2: "Czy wiedziałeś, że coraz częściej sięga się po drony w ochronie środowiska?",
-    3: "Czy wiedziałeś, że drony znajdują coraz powszechniejsze użycie w codziennym życiu, np. przesyłki kurierskie, policyjne?",
-    4: "Czy kiedykolwiek miałeś styczność z wykorzystaniem dronów jak w poprzednich pytaniach?",
-    5: "Czy uważasz, że rozpowszechnienie dronów może stworzyć zagrożenia, które obecnie nie występują?",
-    6: "Czy uważasz, że drony powinny być używane do sztucznego kształtowania systemów biologicznych?",
-    7: "Czy uważasz, że wykorzystanie dronów w eksplorowaniu ciężko dostępnych miejsc, może mieć negatywny wpływ na tamtejsze środowisko?",
-    8: "Czy uważasz, że drony mogą stanowić zagrożenie dla dzikich zwierząt?",
-    9: "Czy uważasz, że wykorzystanie dronów w monitorowaniu pól rolniczych, lasów oraz nabrzeży może budzić dyskomfort u osób przebywających na wymienionych terenach?",
-    10: "Czy uważasz, że drony są tańszym rozwiązaniem niż obecnie używane środki w ochronie środowiska?",
-    11: "Czy uważasz, że drony w ochronie środowiska powinny być eksploatowane przez prywatne instytucje?",
-    12: "Czy uważasz, że wykorzystanie dronów w ochronie środowiska powinno zostać dofinansowane przez państwo?",
-    13: "Czy uważasz, że powinno się zwiększyć wykorzystanie dronów w ochronie środowiska oraz monitorowaniu zagrożeń z tym związanych?",
-    14: "Czy uważasz, że wykorzystanie dronów przynosi więcej korzyści niż zagrożeń?",
-    15: "Czy uważasz, że używanie dronów w ochronie środowiska jest przyszłościowe?",
+    0: "Czy jest dla Ciebie ważna ochrona środowiska?",
+    1: "Czy wiedziałeś, że coraz częściej sięga się po drony w ochronie środowiska?",
+    2: "Czy wiedziałeś, że drony znajdują coraz powszechniejsze użycie w codziennym życiu, np. przesyłki kurierskie, policyjne?",
+    3: "Czy kiedykolwiek miałeś styczność z wykorzystaniem dronów jak w poprzednich pytaniach?",
+    4: "Czy uważasz, że rozpowszechnienie dronów może stworzyć zagrożenia, które obecnie nie występują?",
+    5: "Czy uważasz, że drony powinny być używane do sztucznego kształtowania systemów biologicznych?",
+    6: "Czy uważasz, że wykorzystanie dronów w eksplorowaniu ciężko dostępnych miejsc, może mieć negatywny wpływ na tamtejsze środowisko?",
+    7: "Czy uważasz, że drony mogą stanowić zagrożenie dla dzikich zwierząt?",
+    8: "Czy uważasz, że wykorzystanie dronów w monitorowaniu pól rolniczych, lasów oraz nabrzeży może budzić dyskomfort u osób przebywających na wymienionych terenach?",
+    9: "Czy uważasz, że drony są tańszym rozwiązaniem niż obecnie używane środki w ochronie środowiska?",
+    10: "Czy uważasz, że drony w ochronie środowiska powinny być eksploatowane przez prywatne instytucje?",
+    11: "Czy uważasz, że wykorzystanie dronów w ochronie środowiska powinno zostać dofinansowane przez państwo?",
+    12: "Czy uważasz, że powinno się zwiększyć wykorzystanie dronów w ochronie środowiska oraz monitorowaniu zagrożeń z tym związanych?",
+    13: "Czy uważasz, że wykorzystanie dronów przynosi więcej korzyści niż zagrożeń?",
+    14: "Czy uważasz, że używanie dronów w ochronie środowiska jest przyszłościowe?",
 }
 
-for row in range (0,14):
+for row in range (0,15):
     quesAns = pd.DataFrame(result.apply(pd.Series.value_counts, axis=1).fillna(0))
     rowAns = quesAns.iloc[row].to_frame().T
     rowAns.columns=['Zdecydowanie tak', 'Raczej tak', 'Nie wiem', 'Raczej nie', 'Zdecydowanie nie']
     rowAns = rowAns.astype('int')
     f, ax = plt.subplots(figsize=(15,6))
-    rows = sns.barplot(data=rowAns).set(title=quesDict[row+1])
+    rows = sns.barplot(data=rowAns).set(title=quesDict[row])
     plt.yticks([5,10,15,20,25,30])
     plt.show()
 
 #Answers distribution for people
 ankStatus = ankietowany['Status'].value_counts().to_frame().T
-sns.barplot(data=ankStatus)
+ankStatus.columns = ['Student', 'Pracownik etatowy', 'Uczeń']
+ankStat = sns.barplot(data=ankStatus)
 plt.show()
 
 #Get answers of only students
 answers=pd.DataFrame()
-res = []
 for x in range(1,countCol):
     if x in ankStudent['ID']:
         if x in result.columns:
@@ -178,7 +182,6 @@ plt.show()
 
 #Get answers of only workers
 answersPE=pd.DataFrame()
-resPE = []
 for x in range(1,countCol):
     if x in ankPE['ID']:
         if x in result.columns:
@@ -204,4 +207,28 @@ answerCountPE = {'Zdecydowanie tak': ansPECount1, 'Raczej tak': ansPECount2, 'Ni
 dfAnswerPE = pd.DataFrame.from_dict(answerCountPE, orient='index', columns = ['Ilosc odpowiedzi']).T
 f, ax = plt.subplots(figsize=(10,7))
 sns.barplot(data=dfAnswerPE).set(title="Odpowiedzi pracowników etatowych")
+plt.show()
+
+#Get all answers for particular questions
+nrAns = pd.DataFrame(result.values).T
+nrAns.columns = ['NrPytania_1', 'NrPytania_2', 'NrPytania_3', 'NrPytania_4', 'NrPytania_5', 'NrPytania_6', 'NrPytania_7', 'NrPytania_8', 'NrPytania_9', 'NrPytania_10', 'NrPytania_11', 'NrPytania_12', 'NrPytania_13', 'NrPytania_14', 'NrPytania_15']
+
+ankietowanyAns = pd.concat([ankietowany, nrAns], axis=1, join="inner")
+ankietowanyAns = ankietowanyAns.drop(['Timestamp'], axis=1)
+ankietowanyAns.to_csv('static/allAns.csv', encoding='cp1252', sep=";", index=False)
+
+
+'''
+#Sklearn
+col_values = ankietowanyAns.columns
+ankietowanyAns1 = ankietowanyAns
+encoder = preprocessing.LabelEncoder()
+for col in col_values:
+    ankietowanyAns1[col] = encoder.fit_transform(ankietowany[col])
+    print(encoder.classes_)
+print(ankietowanyAns1)
+'''
+
+plt.figure(figsize=(20,20),dpi = 100)
+sns.heatmap(ankietowanyAns.corr(),annot = ankietowanyAns.corr())
 plt.show()
